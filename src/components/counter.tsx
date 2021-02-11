48f95308c73e7f9d656a0f210a2ab501ec752144
Jose douglas ramirez <chibchombiano26@hotmail.com>
2021-02-11 18:20:57 -0500

    memoized

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,25 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 
 const Counter = () => {
   const [value, setValue] = useState<number>(0);
   const [minimumReached, setMinimumReached] = useState<boolean>(false);
+  const [showLimitReached, setShowLimitReached] = useState<boolean>(false);
 
   useEffect(() => {
-    console.log(value);
     value === 0 ? setMinimumReached(true) : setMinimumReached(false);
+    value === 10 && setShowLimitReached(true);
   }, [value]);
+
+  const label = useMemo(() => {
+    return (
+      <span> {showLimitReached ? `limit reached` : "limit not reached"}</span>
+    );
+  }, []);
 
   return (
     <>
-      <button onClick={() => setValue(value + 1)}>Increase</button>
-      <span style={{ marginLeft: "5px" }}>{value}</span>
-      <button
-        disabled={minimumReached}
-        style={{ marginLeft: "5px" }}
-        onClick={() => setValue(value + -1)}
-      >
-        Decrease
-      </button>
+      <div>
+        <button onClick={() => setValue(value + 1)}>Increase</button>
+        <span style={{ marginLeft: "5px" }}>{value}</span>
+        <button
+          disabled={minimumReached}
+          style={{ marginLeft: "5px" }}
+          onClick={() => setValue(value + -1)}
+        >
+          Decrease
+        </button>
+      </div>
+      {label}
     </>
   );
 };
